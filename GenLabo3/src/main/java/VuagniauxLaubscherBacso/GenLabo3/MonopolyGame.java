07fc38f9e3cfa33bf009d2e355c0ac62f666a58c
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-04-12 11:53:47 +0200

    fixed issue with constructor of Player needing a Piece instead of creating it. Fixed code for MonopolyGame class

--- GenLabo3/src/main/java/VuagniauxLaubscherBacso/GenLabo3/MonopolyGame.java
+++ GenLabo3/src/main/java/VuagniauxLaubscherBacso/GenLabo3/MonopolyGame.java
@@ -6,20 +6,30 @@ import java.util.ArrayList;
 public class MonopolyGame {
     private int roundCnt;
     private final int nbRounds = 20;
+
     private Board board;
     private ArrayList<Player> players;
 
+    private final int nbDie = 2;
+    private ArrayList<Die> dies;
+
     public MonopolyGame(int nbPlayers) throws Exception {
         if (nbPlayers >= 2 && nbPlayers <= 8) {
+            this.board = new Board(40);
+
+            this.dies = new ArrayList<Die>();
+            this.dies.add(new Die());
+            this.dies.add(new Die());
+
             this.players = new ArrayList<Player>();
             for (int i = 0; i < nbPlayers; ++i) {
-                this.players.add(new Player("player " + i));
+                this.players.add(new Player("player " + i, this.dies, this.board));
             }
         } else {
             throw new Exception("There must be at least 2 players and at most 8 players!");
         }
 
-        board = new Board();
+
     }
 
     public void playGame() {
--- GenLabo3/src/main/java/VuagniauxLaubscherBacso/GenLabo3/Player.java
+++ GenLabo3/src/main/java/VuagniauxLaubscherBacso/GenLabo3/Player.java
@@ -1,55 +1,52 @@
 package VuagniauxLaubscherBacso.GenLabo3;
 
+import java.util.ArrayList;
+
 public class Player {
 
-	private String name;
-	private Die[] dies;
-	private Piece piece;
-	private Board board;
+    private String name;
+    private ArrayList<Die> dies;
+    private Piece piece;
+    private Board board;
 
-	
-	public Player(String name, Die[] dies, Piece piece, Board board) {
-		this.name 		= name;
-		this.dies 		= dies;
-		this.piece 		= piece;
-		this.board 		= board;
 
-	}	
+    public Player(String name, ArrayList<Die> dies, Board board) {
+        this.name = name;
+        this.dies = dies;
+        this.piece = new Piece(board.getFirstSquare());
+        this.board = board;
 
-	public String getName() {
-		return name;
-	}
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
-	
-	public void takeTurn(){
-		
-		Square oldLoc, newLoc;
-		int fv = 0;
-		
-		for(Die die: dies) {
-			die.roll();
-			fv += die.getFaceValue();
-		}
-		
-		try {
-			oldLoc = piece.getLocation();
-			newLoc = board.getSquare(oldLoc,fv);
-			piece.setLocation(newLoc);
-			
-		} catch (Exception e) {
-			oldLoc = board.getFirstSquare();
-			newLoc = board.getSquare(oldLoc,fv);
-			piece.setLocation(newLoc);
-			e.printStackTrace();
-		}
-		
-		
-		
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void takeTurn() {
+
+        Square oldLoc, newLoc;
+        int fv = 0;
+
+        for (Die die : dies) {
+            die.roll();
+            fv += die.getFaceValue();
+        }
+
+        try {
+            oldLoc = piece.getLocation();
+            newLoc = board.getSquare(oldLoc, fv);
+            piece.setLocation(newLoc);
+
+        } catch (Exception e) {
+            oldLoc = board.getFirstSquare();
+            newLoc = board.getSquare(oldLoc, fv);
+            piece.setLocation(newLoc);
+            e.printStackTrace();
+        }
+    }
 
 }
